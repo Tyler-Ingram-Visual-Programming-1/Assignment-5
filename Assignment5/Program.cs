f28681f6b74ea0d93191e0967e2bfade53ff1679
Tyler Ingram <tylerphx@gmail.com>
2021-03-19 15:21:04 -0500

    - Created UI with basic functionality

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Assignment5
 {
-    class Program
+    static class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
     }
 }
